9b97a2489a6f98c8ff07d92c8144288d7757e0cb
marc.bideau <marc.bideau9@gmail.com>
2022-02-10 16:58:11 +0100

    push premiere api toujours a débug mais bon

---
+++ projet_Netflix/Controleur/connexion.php
@@ -0,0 +1,7 @@
+<?php
+  $server = "localhost";
+  $username = "root";
+  $password = "";
+  $db = "projet_netflix";
+  $conn = mysqli_connect($server, $username, $password, $db);
+?>
---
+++ projet_Netflix/Controleur/getFilms.php
@@ -0,0 +1,51 @@
+<?php
+require("connexion.php");
+$request_method = $_SERVER["REQUEST_METHOD"];
+  
+switch($request_method)
+  {
+    case 'GET':
+      if(!empty($_GET["id"]))
+      {
+        // Récupérer un seul produit
+        $id = $_GET["id"];
+        getProduct($id);
+      }
+      else
+      {
+        // Récupérer tous les produits
+        getProducts();
+      }
+      break;
+  }
+
+  function getProducts()
+  {
+    global $conn;
+    $query = "SELECT * FROM film ";
+    $response = array();
+    $result = mysqli_query($conn, $query);
+    while($row = mysqli_fetch_assoc($result))
+    {
+      $response[] = $row;
+    }
+    echo json_encode($response,JSON_PRETTY_PRINT);
+    
+  }
+  function getProduct($id="")
+  {
+    global $conn;
+    $query = "SELECT * FROM film";
+    if($id != '')
+    {
+      $query .= " WHERE titre='".$id."' LIMIT 1";
+    }
+    $response = array();
+    $result = mysqli_query($conn, $query);
+    while($row = mysqli_fetch_assoc($result))
+    {
+      $response[] = $row;
+    }
+      echo json_encode($response);
+  }
+?>
