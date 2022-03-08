10fcd7cc8aacdc3caf58cc9656e099aec18c02c8
marc.bideau <marc.bideau9@gmail.com>
2022-03-08 11:53:19 +0100

    inscription

---
+++ projet_Netflix/Controleur/inscription.php
@@ -0,0 +1,56 @@
+<?php
+require("connexion.php");
+$request_method = $_SERVER["REQUEST_METHOD"];
+
+  
+switch($request_method)
+  {
+    case 'POST':
+      if(!empty($_GET["admin"]))
+      {
+        // Récupérer un seul produit
+        $id = $_GET["admin"];
+        inscriptonAdmin($id);
+      }
+      else
+      {
+        // Récupérer tous les produits
+        inscriptionUser();
+      }
+      break;
+  }
+
+
+
+
+function inscriptionUser(){
+    global $conn;
+
+    $data = json_decode(file_get_contents('php://input'),true);
+    $username = $data['name'];
+    $mdp = $data['mdp'];
+    $request = "INSERT INTO `user`(`login`, `mot_de_passe`) VALUES ('$username', '$mdp')";
+    $response = array();
+    if ($result=mysqli_query($conn,$request)) {
+        echo '{"state":"inscrit"}';
+    } 
+    else echo '{"state":"déja existant"}';
+    
+}
+
+function inscriptonAdmin($admin=''){
+    global $conn;
+
+    $data = json_decode(file_get_contents('php://input'),true);
+    $username = $data['name'];
+    $mdp = $data['mdp'];
+    $request = "INSERT INTO `user`(`login`, `mot_de_passe`, `admin`) VALUES ('$username', '$mdp','$admin')";
+    $response = array();
+    if ($result=mysqli_query($conn,$request)) {
+        echo '{"state":"inscrit"}';
+    } 
+    else echo '{"state":"déja existant"}';
+
+}
+
+?>
