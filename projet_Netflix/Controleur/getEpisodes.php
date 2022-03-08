fe26246bf42637819a40ffbe6945ef7c34cde263
marc.bideau <marc.bideau9@gmail.com>
2022-03-08 14:35:10 +0100

    Update getEpisodes.php

--- projet_Netflix/Controleur/getEpisodes.php
+++ projet_Netflix/Controleur/getEpisodes.php
@@ -3,7 +3,6 @@ require('connexion.php');
     $id=$_GET['id'];
     global $conn;
     $query = "SELECT * FROM episode where id_serie='$id'";
-    echo $query;
     $response = array();
     $result = mysqli_query($conn, $query);
     while($row = mysqli_fetch_assoc($result))
