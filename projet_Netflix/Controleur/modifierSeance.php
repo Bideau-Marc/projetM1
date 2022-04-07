32b6cf4b16025ff1f1a3f5b9b89cbb01673e9ee3
marc.bideau <marc.bideau9@gmail.com>
2022-04-07 14:53:57 +0200

    sommaire

---
+++ projet_Netflix/Controleur/modifierSeance.php
@@ -0,0 +1,17 @@
+<?php
+    require('connexion.php');
+    global $conn;
+    $data = json_decode(file_get_contents('php://input'),true);
+    $idSeance = $data['idSeance'];
+    $dateSeance = $data['dateSeance'];
+    $titreSeance = $data['titreSeance'];
+    $idFilm = $data['idFilm'];
+    $idSerie = $data['idSerie'];
+    $nbPlace = $data['nbPlace'];
+    $request = "UPDATE `seance` SET `date`='$dateSeance',`titre_seance`='$titreSeance',`id_film`='$idFilm',`id_serie`='$idSerie',`nb_place`='$nbPlace' WHERE id_seance = '$idSeance'";
+    $response = array();
+    if ($result=mysqli_query($conn,$request)) {
+        echo '{"state":"seance updaté"}';
+    } 
+    else echo '{"state":"seance non update"}';
+?>
