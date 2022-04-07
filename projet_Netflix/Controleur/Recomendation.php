8555e9b7eb40185f69c004cad64184835c0d6ab3
marc.bideau <marc.bideau9@gmail.com>
2022-04-07 14:48:50 +0200

    Update Recomendation.php

--- projet_Netflix/Controleur/Recomendation.php
+++ projet_Netflix/Controleur/Recomendation.php
@@ -1,9 +1,8 @@
 <?php
     require('connexion.php');
     global $conn;
-    // $data = json_decode(file_get_contents('php://input'),true);
-    // $id = $data['id'];
-    $id=12;
+    $data = json_decode(file_get_contents('php://input'),true);
+    $id = $data['id'];
     $cpt = 0;
     $requete = "select  count(case c.id when 1 then 1 else null end) as action, count(case c.id when 2 then 1 else null end ) as fiction, count(case c.id when 3 then 1 else null end) as humour, count(case c.id when 4 then 1 else null end) as horreur, count(case c.id when 5 then 1 else null end) as amour from film f, favori ff, categorie c, user u where f.categorie = c.id and  f.id = ff.id_film and u.id ='$id' and u.id = ff.idUtilisateur;";
     $result = mysqli_query($conn, $requete);
