6e5d1f0791d2265f0b8e503e3f5e07b38676c0f8
Julien-peria <77395868+Julien-peria@users.noreply.github.com>
2022-04-07 15:42:51 +0200

    bug

--- projet_Netflix/Controleur/getUserFromSeance.php
+++ projet_Netflix/Controleur/getUserFromSeance.php
@@ -7,7 +7,7 @@ $request_method = $_SERVER["REQUEST_METHOD"];
     $data = json_decode(file_get_contents('php://input'),true);
     $idSeance = $data['idSeance'];
     
-    $query = "SELECT * FROM reservation r, seance s, user u, film fi where s.id_seance = '$idSeance' and r.id_user = u.id and fi.id=s.id_film and r.id_seance = s.id_seance";   
+    $query = "SELECT *, r.id as id_reservation,u.id as id_user FROM reservation r, seance s, user u, film fi where s.id_seance = '$idSeance' and r.id_user = u.id and fi.id=s.id_film and r.id_seance = s.id_seance";   
     $response = array();
     $result = mysqli_query($conn, $query);
     while($row = mysqli_fetch_assoc($result))
