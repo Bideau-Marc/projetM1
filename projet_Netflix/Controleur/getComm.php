100af014af1f0508b129331b5024dd650dcde61c
marc.bideau <marc.bideau9@gmail.com>
2022-05-05 14:41:46 +0200

    Update getComm.php

--- projet_Netflix/Controleur/getComm.php
+++ projet_Netflix/Controleur/getComm.php
@@ -2,7 +2,7 @@
 require('connexion.php');
     $id=$_GET['id'];
     global $conn;
-    $query = "SELECT * FROM `commentaire` c, user u  WHERE u.id = c.id_user";
+    $query = "SELECT top* FROM `commentaire` c, user u  WHERE u.id = c.id_user limit 2";
     $response = array();
     $result = mysqli_query($conn, $query);
     while($row = mysqli_fetch_assoc($result))
